e864d585c8fb7e75274eef333b090c86f6b26f16
Mark Hernandez <m.hern1415@gmail.com>
2019-09-10 21:04:15 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,10 +22,10 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = 1)
+def can_be_created_in_a_block(m.title = "Home Alone", m.release_date = 1990)
   Movie.create do |m|
-  m.title = "Home Alone"
-    m.release_date = 1990
+  m.title = "The Room"
+    m.release_date = 2003
   end
 end
 
